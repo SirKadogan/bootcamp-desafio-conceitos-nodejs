25ccf75d0c05026c0ba1aed8275be5bf2a33e330
SirKadogan <thaleskenne@hotmail.com>
2020-04-14 20:50:46 -0300

    Challenge finished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,22 +12,61 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(newRepository);
+
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+  if (index < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+
+  const updatedRepository = { ...repositories[index], title, url, techs };
+  repositories[index] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+  if (index < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = repositories.findIndex((repository) => repository.id === id);
+  if (index < 0) {
+    return response.status(400).json({ message: "Repository not found" });
+  }
+  let updatedRepository = repositories[index];
+  updatedRepository.likes = updatedRepository.likes + 1;
+
+  return response.json(updatedRepository);
 });
 
 module.exports = app;
